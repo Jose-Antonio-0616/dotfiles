2c05934132d39ffa42e3da70208bf4e9c343b6f7
Jose (Kael OS) <jose@kael.local>
2026-07-12 20:45:00 -0400

    Auto-backup: 2026-07-12 20:45:00 (3 cambios)

--- .config/qtile/layouts.py
+++ .config/qtile/layouts.py
@@ -19,7 +19,7 @@ layout_theme = {
 layouts = [
     layout.MonadTall(**layout_theme),
     layout.MonadWide(**layout_theme),
-    layout.Bsp(**layout_theme),
+    layout.Bsp(**layout_theme, fair=False),
     layout.Max(border_width=0, margin=0),
     layout.Spiral(**layout_theme),
     layout.Matrix(**layout_theme, columns=2),
