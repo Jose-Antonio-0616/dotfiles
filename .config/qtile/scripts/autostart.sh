396a1cc115234513f38bb86f43fcd76d714d2e47
Jose (Kael OS) <jose@kael.local>
2026-07-13 21:23:57 -0400

    Auto-backup: 2026-07-13 21:23:57 (3 cambios)

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -11,7 +11,7 @@ else
 fi
 
 # background
-feh --bg-center --image-bg black /home/jose/.config/qtile/wallpaper/rasta_2.jpeg &
+feh --bg-center --image-bg black /home/jose/.config/qtile/wallpaper/rasta_3.jpeg &
 
 # GTK live theme updates
 xsettingsd &
